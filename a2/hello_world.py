71765d2d219c279733e609ce5b3c75417800305e
DewinDavis <59937729+DewinDavis@users.noreply.github.com>
2020-01-17 14:26:55 -0500

    Create hello_world.py

---
+++ a2/hello_world.py
@@ -0,0 +1,20 @@
+"""
+I'm 23 years old
+Coding is my downfall 
+I'm only child 
+"""
+
+# declare some vars
+FavTeam = 'Ravens'
+FavColor = 'Red'
+Car = 'Mustang'
+Age = 23
+
+#These are my output statements
+
+print('Hello world, my name is Dewin')
+
+print('My favorite team is ' + FavTeam)
+print('My favorite color is ' + FavColor)
+print('Car ' + Car)
+print('My age is ' + str(Age))
